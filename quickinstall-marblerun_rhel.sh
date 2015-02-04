cf44ab9584a3d35feec2c67a97a4135a90045c47
root <root@apsd-jo01.ngid.centurylink.net>
2015-02-04 13:03:59 -0700

    Added more to quick installer

--- quickinstall-marblerun_rhel.sh
+++ quickinstall-marblerun_rhel.sh
@@ -13,10 +13,14 @@ mkdir -p /opt/MarbleRun/conf.d
 mkdir -p /opt/MarbleRun/examples
 mkdir -p /etc/marblerun
 ln -s /opt/MarbleRun/conf.d /etc/marblerun/
+ln -s /opt/MarbleRun/daemons/mrmon.py /usr/bin/mr-monitor
+chmod +x /usr/bin/mr-monitor
 echo '{"path": "/opt/MarbleRun/daemons/mrmond.py","dir": "/tmp","instances": "<[cores]>-1"}' > /etc/marblerun/conf.d/01-monitor.json
 cp daemons/* /opt/MarbleRun/daemons/
 cp examples/* /opt/MarbleRun/examples/
 chmod +x /opt/MarbleRun/daemons/*
+cat tools/mr.py > /usr/bin/mr
+chmod +x /usr/bin/mr
 cd src
 python setup.py install
 cd /tmp
